57f2227ba1d27cf8ef417ec97735166e8ba4f9ff
Tr1flic <132134370+Tr1flic@users.noreply.github.com>
2023-05-06 14:32:28 -0400

    rep system beta and help page
    rep system beta and help page added

--- syn.py
+++ syn.py
@@ -10,15 +10,21 @@ intents.members = True
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-skid_role_id  = 0   #CHANGE THIS!!!!!!!!!!!!!!!!!!
-mod_role_id   = 0   #CHANGE THIS!!!!!!!!!!!!!!!!!!
-log_channel_id= 0   #CHANGE THIS!!!!!!!!!!!!!!!!!!
+skid_role_id  = 1104393023149834323   #CHANGE THIS!!!!!!!!!!!!!!!!!!
+mod_role_id   = 1104374965136011314   #CHANGE THIS!!!!!!!!!!!!!!!!!!
+log_channel_id= 1104418931063664730   #CHANGE THIS!!!!!!!!!!!!!!!!!!
 
 try:
   muted = open("muted_users", "r").read().strip().split("\n")
 except:
   open("muted_users", "w").write("")
   muted = []
+
+try:
+  reps = open("reps", "r").read().strip().split("\n")
+except:
+  open("reps", "w").write("")
+  reps = []
 
 async def faq(message):
         faq_section = """
@@ -46,8 +52,32 @@ ______      ____
       """.strip()
         await message.channel.send(faq_section)
 
+async def help_page(message):
+        help_page_text = """
+```
+_    _ ______ _      _____
+| |  | |  ____| |    |  __ \\
+| |__| | |__  | |    | |__) |
+|  __  |  __| | |    |  ___/
+| |  | | |____| |____| |
+|_|  |_|______|______|_|
+
++rep: reply to a message with this command and the user will get +1 reputation.
+
+-rep: Reply to a message with this command and the user will get -1 reputation.
+
+!rep *<user id>: If no user id is provided it displays your own reputation and if one is provided, it displays the users reputation.
+
+!help: Displays this.
+
+!faq: Displays frequently asked questions in here server.
+```
+      """.strip()
+        await message.channel.send(help_page_text)
+
 @bot.event
 async def on_message(message):
+        ctx = await bot.get_context(message)
         words = [
         "dox",
         "dos",
@@ -79,16 +109,73 @@ async def on_message(message):
                   channel = bot.get_channel(log_channel_id)
                   await channel.send("The user " + str(message.author.id) + " sent message which was flagged:\n " + message.content)
 
-        if message.content == "!faq":
+        if message.content == "!help":
+            await help_page(message)
+
+        elif message.content == "!faq":
             await faq(message)
+
+        elif message.content == "!rep":
+            try:
+              index_loc = reps.index(str(message.author.id))
+              await message.channel.send("The user:" + str(message.author) + " has " + reps[index_loc+1] + " reputation.")
+            except:
+              await message.channel.send("Username doesn't exist or doesn't have rep yet.")
+
+        elif message.content.startswith("!rep"):
+              try:
+                index_loc = reps.index(message.content.split("!rep")[1].strip())
+                to_rep = int(message.content.split("!rep")[1].strip())
+                name = await bot.fetch_user(to_rep)
+                await message.channel.send("The user:" + str(name) + " has " + reps[index_loc+1] + " reputation.")
+              except:
+                await message.channel.send("Username doesn't exist or doesn't have rep yet.")
+
+        elif message.content.startswith("!addrep"):
+            to_rep = int(message.content.split("!addrep")[1].strip())
+            if message.author.id == to_rep or to_rep == bot.user.id:
+                return
+            try:
+              index_loc = reps.index(str(to_rep))
+              reps[index_loc+1] = str(int(reps[index_loc+1])+1)
+            except:
+              try:
+                name = await bot.fetch_user(to_rep)
+                print("Name:" + str(name))
+                reps.append(str(to_rep))
+                reps.append(str(1))
+              except:
+                await message.channel.send("The user:" + str(to_rep) + " doesn't exist.")
+
+        elif message.content == "+rep":
+            reply_to = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+            if message.author.id == reply_to.author.id or reply_to.author.id == bot.user.id:
+                return
+            try:
+              index_loc = reps.index(str(reply_to.author.id))
+              reps[index_loc+1] = str(int(reps[index_loc+1])+1)
+            except:
+              reps.append(str(reply_to.author.id))
+              reps.append(str(1))
+
+        elif message.content == "-rep":
+            reply_to = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+            if message.author.id == reply_to.author.id or reply_to.author.id == bot.user.id:
+                return
+            try:
+              index_loc = reps.index(str(reply_to.author.id))
+              reps[index_loc+1] = str(int(reps[index_loc+1])-1)
+            except:
+              reps.append(str(reply_to.author.id))
+              reps.append(str(-1))
 
         if mod_role_id in [role.id for role in message.author.roles]:
             if message.content.startswith("!modhelp"):
-                help_page = """
+                mod_help_page = """
 !unmute userid: Unmute muted user.
 !mute userid: Mute a user
                 """.strip()
-                await message.channel.send(help_page)
+                await message.channel.send(mod_help_page)
             elif message.content.startswith("!unmute"):
                 muted.remove(''.join(message.content.split("!unmute")[1:]).strip())
                 await message.channel.send("The user \"" + ''.join(message.content.split("!unmute")[1:]).strip() + "\" has been unmuted by the staff!")
@@ -98,7 +185,9 @@ async def on_message(message):
 
         if muted != open("muted_users", "rb").read().strip().split(b"\n"):
             open("muted_users", "w").write('\n'.join(muted))
+        print(reps)
+        if '\n'.join(reps) != open("reps", "r").read().strip().split("\n"):
+            open("reps", "w").write('\n'.join(reps))
 
 tok = os.environ['key']
-print(tok)
 bot.run(tok)
